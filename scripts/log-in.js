930579081cc2ccac468f02aa834b1d912a9ae506
Vassilia <vassilia@gerodimos.com>
2025-09-15 15:04:54 +0200

    worked on the log-in-site

--- scripts/log-in.js
+++ scripts/log-in.js
@@ -3,10 +3,12 @@ document.addEventListener('DOMContentLoaded', moveLogoToTopLeft);
 function moveLogoToTopLeft() {
     const logo = document.getElementById('logo');
     const logInContainer = document.getElementById('log-in-container');
+    const registryContainer = document.getElementById('registry-container');
     if (logo) {
         setTimeout(() => {
             logo.classList.add('logo-move-to-top-left');
             logInContainer.classList.remove('d-none');
+            registryContainer.classList.remove('d-none');
         }, 500);
     };
 }
