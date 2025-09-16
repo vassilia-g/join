998e87771d51806190287b799bb370989689aaa2
Marco Rößler <marco_r1985@yahoo.de>
2025-09-16 15:54:25 +0200

    added sign up and checkbox; added svg´sfor sign up; added style to the log-in.css; added functions to the log-in.js

--- scripts/log-in.js
+++ scripts/log-in.js
@@ -13,3 +13,30 @@ function moveLogoToTopLeft() {
     };
 }
 
+function showSignUpContainer() {
+    const signUpContainer = document.getElementById("sign-up-container");
+    const logInContainer = document.getElementById("log-in-container");
+    const registryContainer = document.getElementById("registry-container");
+    if (signUpContainer) {
+        signUpContainer.classList.remove("d-none");
+        logInContainer.classList.add("d-none");
+        registryContainer.classList.add("d-none");
+    }
+}
+
+function enableSignUpButton() {
+    const checkbox = document.getElementById("checkbox");
+    const signUpButton = document.getElementById("sign-up-form-button");
+    
+    if (checkbox && signUpButton) {
+        if (checkbox.checked) {
+            signUpButton.disabled = false;
+            signUpButton.classList.remove("disabled-sign-up-form-button");
+            signUpButton.classList.add("enabled-sign-up-form-button");
+        } else {
+            signUpButton.disabled = true;
+            signUpButton.classList.add("disabled-sign-up-form-button");
+            signUpButton.classList.remove("enabled-sign-up-form-button");
+        }
+    }
+}
