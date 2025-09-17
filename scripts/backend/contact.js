f9ed028e36ba7a3312daca24dbf3105e53493666
Vassilia <vassilia@gerodimos.com>
2025-09-17 23:41:55 +0200

    created a folder with files for the backend

---
+++ scripts/backend/contact.js
@@ -0,0 +1,7 @@
+class Contact {
+    constructor(id, user_owner_id, friend_user_id) {
+        this.id = id;
+        this.user_owner_id = user_owner_id;
+        this.friend_user_id = friend_user_id;
+    }
+}
---
+++ scripts/backend/user.js
@@ -0,0 +1,11 @@
+class User {
+    constructor(id, username, password, email, forename, lastname, status) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.forename = forename;
+        this.lastname = lastname;
+        this.status = status;
+    }
+}
