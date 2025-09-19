b18e5768e0ba7bf11e92501adc97bffc32c45e55
Jule Zieten <ju-zie@web.de>
2025-09-19 15:04:27 +0200

    global.css content margin changed (has to be on the left), in add task assigned to dropdown changed + js functions and example array

--- scripts/add-task.js
+++ scripts/add-task.js
@@ -11,6 +11,11 @@ const mediumSvgPath = mediumSvg.querySelectorAll("path");
 const lowButton = document.getElementById('low-priority-btn');
 const lowSvg = document.getElementById('low-svg');
 const lowSvgPath = lowSvg.querySelectorAll("path");
+const contactsToSelect = document.getElementById('contacts-to-select');
+const selectedContacts = document.getElementById('selected-contacts');
+const dropdownIcon = document.getElementById('dropdown-icon');
+let contacts = ['Jule Zieten', 'Marco Rößler', 'Vassilia Gerodimos', 'Anika Schmidt', 'Dustin Condello'];
+
 
 const picker = flatpickr("#task-due-date", {
   dateFormat: "d.m.Y",
@@ -172,4 +177,103 @@ function changeUrgentBtnToDefault() {
   urgentSvgPath.forEach(path => {
     path.setAttribute("fill", "#FF3D00");
   });
+}
+
+function openDropdownContacts() {
+  if (contactsToSelect.innerHTML == '') {
+    for (let i = 0; i < contacts.length; i++) {
+      contactsToSelect.innerHTML += showContacts(i);
+    }
+    setTimeout(() => {
+      contactsToSelect.classList.add('show');
+    }, 10);
+  } else {
+    contactsToSelect.classList.remove('show');
+    setTimeout(() => {
+      contactsToSelect.innerHTML = '';
+    }, 300);
+    showSelectedContacts();
+  }
+  dropdownIcon.classList.toggle("open");
+}
+
+function showContacts(i) {
+  let initials = getInitials(contacts[i]);
+  return `
+        <div class="single-contact">
+          <div class="contact-name">
+            <svg id="initials-${i}" width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="21" cy="21" r="20" fill="#00BEE8" stroke="white" stroke-width="2"/>
+              <text x="50%" y="50%" text-anchor="middle" dominant-baseline="central" font-size="14" fill="white">
+              ${initials}</text>
+            </svg>
+            <span>${contacts[i]}</span>
+          </div>
+          <div class="contact-checkbox" id="checkbox-${i}">
+            <svg onclick="checkContact(${i})" width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <rect x="4.38818" y="4" width="16" height="16" rx="3" stroke="#2A3647" stroke-width="2"/>
+            </svg>
+          </div>
+        </div>
+      `;
+}
+
+function getInitials(name) {
+  const parts = name.split(" ");
+  const first = parts[0].charAt(0);
+  const last = parts.length > 1 ? parts[parts.length - 1].charAt(0) : "";
+  return (first + last).toUpperCase();
+}
+
+
+function checkContact(i) {
+  let checkbox = document.getElementById(`checkbox-${i}`);
+  let initials = document.getElementById(`initials-${i}`)
+
+  if (checkbox.classList.contains("checked")) {
+    initials.classList.remove("checked");
+    checkbox.classList.remove("checked");
+    checkbox.innerHTML = showEmptyCheckbox(i);
+  } else {
+    checkbox.classList.add("checked");
+    initials.classList.add("checked");
+    checkbox.innerHTML = showCheckedCheckbox(i);
+  }
+}
+
+function showEmptyCheckbox(i) {
+  return `
+      <svg onclick="checkContact(${i})" width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect x="4.38818" y="4" width="16" height="16" rx="3" stroke="#2A3647" stroke-width="2"/>
+      </svg>
+    `;
+}
+
+function showCheckedCheckbox(i) {
+  return `
+      <svg onclick="checkContact(${i})" width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M20.3882 11V17C20.3882 18.6569 19.045 20 17.3882 20H7.38818C5.73133 20 4.38818 18.6569 4.38818 17V7C4.38818 5.34315 5.73133 4 7.38818 4H15.3882" stroke="#2A3647" stroke-width="2" stroke-linecap="round"/>
+        <path d="M8.38818 12L12.3882 16L20.3882 4.5" stroke="#2A3647" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+    `;
+}
+
+function showSelectedContacts() {
+  selectedContacts.innerHTML = '';
+  let checkedInititals = document.querySelectorAll("svg.checked");
+  if (checkedInititals.length < 3) {
+    for (let i = 0; i < checkedInititals.length; i++) {
+      selectedContacts.innerHTML += `${checkedInititals[i].outerHTML}`;
+    }
+  } else {
+    for (let i = 0; i < 3; i++) {
+      selectedContacts.innerHTML += `${checkedInititals[i].outerHTML}`;
+    }
+    selectedContacts.innerHTML += `
+      <svg width="40" height="40" viewBox="0 0 42 42" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="21" cy="21" r="20" fill="none" stroke="#2a3647" stroke-width="2"/>
+        <text x="50%" y="50%" text-anchor="middle" dominant-baseline="central" font-size="16" fill="#2a3647">+${checkedInititals.length - 3}</text>
+      </svg>
+    `;
+  }
 }
