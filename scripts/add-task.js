abf77019e2c0b5b8dde06b8081b30b1ba32e95d0
Jule Zieten <ju-zie@web.de>
2025-09-23 14:52:09 +0200

    add task contacts and category dropdown changed, selected Subtasks edited

--- scripts/add-task.js
+++ scripts/add-task.js
@@ -17,6 +17,8 @@ const dropdownIcon = document.getElementById('dropdown-icon');
 const categories = document.getElementById('categories');
 const dropdownIconCategories = document.getElementById('dropdown-icon-categories');
 const categoryInput = document.getElementById('input-category');
+const contactInputAndDropdown = document.getElementById('contact-input-and-dropdown');
+const categoryInputAndDropdown = document.getElementById('category-input-and-dropdown');
 let contacts = ['Jule Zieten', 'Marco Rößler', 'Vassilia Gerodimos', 'Anika Schmidt', 'Dustin Condello'];
 const createTaskButton = document.getElementById("create-task-btn");
 createTaskButton.disabled = true;
@@ -92,8 +94,28 @@ function deleteSubtask() {
 
 function addSubtask() {
   selectedSubtasks.innerHTML += `
-    <ul>
-      <li>${subtaskInput.value}</li>
+    <ul class="subtask-list">
+      <div class="subtask-list-element">
+        <li>${subtaskInput.value}</li>
+        <div class="delete-or-edit-icons">
+          <svg onclick="editSubtask()" class="edit-svg" width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <mask id="mask0_75592_9969" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="25" height="24">
+              <rect x="0.144531" width="24" height="24" fill="#D9D9D9"/>
+            </mask>
+            <g mask="url(#mask0_75592_9969)">
+              <path d="M5.14453 19H6.54453L15.1695 10.375L13.7695 8.975L5.14453 17.6V19ZM19.4445 8.925L15.1945 4.725L16.5945 3.325C16.9779 2.94167 17.4487 2.75 18.007 2.75C18.5654 2.75 19.0362 2.94167 19.4195 3.325L20.8195 4.725C21.2029 5.10833 21.4029 5.57083 21.4195 6.1125C21.4362 6.65417 21.2529 7.11667 20.8695 7.5L19.4445 8.925ZM17.9945 10.4L7.39453 21H3.14453V16.75L13.7445 6.15L17.9945 10.4Z" fill="#2A3647"/>
+            </g>
+          </svg>
+          <svg onclick="deleteSubtask()" width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <mask id="mask0_75592_9951" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="25" height="24">
+              <rect x="0.144531" width="24" height="24" fill="#D9D9D9"/>
+            </mask>
+            <g mask="url(#mask0_75592_9951)">
+              <path d="M7.14453 21C6.59453 21 6.1237 20.8042 5.73203 20.4125C5.34036 20.0208 5.14453 19.55 5.14453 19V6C4.8612 6 4.6237 5.90417 4.43203 5.7125C4.24036 5.52083 4.14453 5.28333 4.14453 5C4.14453 4.71667 4.24036 4.47917 4.43203 4.2875C4.6237 4.09583 4.8612 4 5.14453 4H9.14453C9.14453 3.71667 9.24036 3.47917 9.43203 3.2875C9.6237 3.09583 9.8612 3 10.1445 3H14.1445C14.4279 3 14.6654 3.09583 14.857 3.2875C15.0487 3.47917 15.1445 3.71667 15.1445 4H19.1445C19.4279 4 19.6654 4.09583 19.857 4.2875C20.0487 4.47917 20.1445 4.71667 20.1445 5C20.1445 5.28333 20.0487 5.52083 19.857 5.7125C19.6654 5.90417 19.4279 6 19.1445 6V19C19.1445 19.55 18.9487 20.0208 18.557 20.4125C18.1654 20.8042 17.6945 21 17.1445 21H7.14453ZM7.14453 6V19H17.1445V6H7.14453ZM9.14453 16C9.14453 16.2833 9.24036 16.5208 9.43203 16.7125C9.6237 16.9042 9.8612 17 10.1445 17C10.4279 17 10.6654 16.9042 10.857 16.7125C11.0487 16.5208 11.1445 16.2833 11.1445 16V9C11.1445 8.71667 11.0487 8.47917 10.857 8.2875C10.6654 8.09583 10.4279 8 10.1445 8C9.8612 8 9.6237 8.09583 9.43203 8.2875C9.24036 8.47917 9.14453 8.71667 9.14453 9V16ZM13.1445 16C13.1445 16.2833 13.2404 16.5208 13.432 16.7125C13.6237 16.9042 13.8612 17 14.1445 17C14.4279 17 14.6654 16.9042 14.857 16.7125C15.0487 16.5208 15.1445 16.2833 15.1445 16V9C15.1445 8.71667 15.0487 8.47917 14.857 8.2875C14.6654 8.09583 14.4279 8 14.1445 8C13.8612 8 13.6237 8.09583 13.432 8.2875C13.2404 8.47917 13.1445 8.71667 13.1445 9V16Z" fill="#2A3647"/>
+            </g>
+          </svg>
+        </div>
+      </div>
     </ul>`;
   subtaskInput.value = "";
   subtaskPick.classList.add('d-none');
@@ -189,6 +211,7 @@ function changeUrgentBtnToDefault() {
 }
 
 function openDropdownContacts() {
+  selectedContacts.innerHTML = '';
   if (contactsToSelect.innerHTML == '') {
     for (let i = 0; i < contacts.length; i++) {
       contactsToSelect.innerHTML += showContacts(i);
@@ -197,13 +220,40 @@ function openDropdownContacts() {
       contactsToSelect.classList.add('show');
     }, 10);
   } else {
-    contactsToSelect.classList.remove('show');
-    setTimeout(() => {
-      contactsToSelect.innerHTML = '';
-    }, 300);
+    hideDropdownContacts();
     showSelectedContacts();
   }
   dropdownIcon.classList.toggle("open");
+}
+
+document.onclick = function (event) {
+  const isClickOnSvg = event.target.closest('svg');
+  if (isClickOnSvg) {
+    return;
+  }
+  if (!contactInputAndDropdown.contains(event.target) && contactsToSelect.classList.contains('show')) {
+    hideDropdownContacts();
+    dropdownIcon.classList.toggle("open");
+    showSelectedContacts();
+  }
+  if (!categoryInputAndDropdown.contains(event.target) && categories.classList.contains('show')) {
+    hideDropdownCategories();
+    dropdownIconCategories.classList.toggle("open");
+  }
+}
+
+function hideDropdownContacts() {
+  contactsToSelect.classList.remove('show');
+  setTimeout(() => {
+    contactsToSelect.innerHTML = '';
+  }, 300);
+}
+
+function hideDropdownCategories() {
+  categories.classList.remove('show');
+  setTimeout(() => {
+    categories.innerHTML = '';
+  }, 300);
 }
 
 function showContacts(i) {
@@ -238,7 +288,6 @@ function getInitials(name) {
 function checkContact(i) {
   let checkbox = document.getElementById(`checkbox-${i}`);
   let initials = document.getElementById(`initials-${i}`)
-
   if (checkbox.classList.contains("checked")) {
     initials.classList.remove("checked");
     checkbox.classList.remove("checked");
@@ -269,22 +318,28 @@ function showCheckedCheckbox(i) {
 
 function showSelectedContacts() {
   selectedContacts.innerHTML = '';
-  let checkedInititals = document.querySelectorAll("svg.checked");
-  if (checkedInititals.length < 3) {
-    for (let i = 0; i < checkedInititals.length; i++) {
-      selectedContacts.innerHTML += `${checkedInititals[i].outerHTML}`;
+  let checkedInitials = document.querySelectorAll("svg.checked");
+  if (checkedInitials.length < 3) {
+    for (let i = 0; i < checkedInitials.length; i++) {
+      selectedContacts.innerHTML += `<div class="selected-contacts-svg">${checkedInitials[i].outerHTML}</div>`;
     }
   } else {
     for (let i = 0; i < 3; i++) {
-      selectedContacts.innerHTML += `${checkedInititals[i].outerHTML}`;
+      selectedContacts.innerHTML += `<div class="selected-contacts-svg">${checkedInitials[i].outerHTML}</div>`;
     }
-    selectedContacts.innerHTML += `
-      <svg width="40" height="40" viewBox="0 0 42 42" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="21" cy="21" r="20" fill="none" stroke="#2a3647" stroke-width="2"/>
-        <text x="50%" y="50%" text-anchor="middle" dominant-baseline="central" font-size="16" fill="#2a3647">+${checkedInititals.length - 3}</text>
-      </svg>
-    `;
+    selectedContacts.innerHTML += showMoreContacts(checkedInitials);
   }
+}
+
+function showMoreContacts(checkedInitials) {
+  return `
+      <div class="selected-contacts-svg">
+        <svg width="40" height="40" viewBox="0 0 42 42" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="21" cy="21" r="20" fill="none" stroke="#2a3647" stroke-width="2"/>
+          <text x="50%" y="50%" text-anchor="middle" dominant-baseline="central" font-size="16" fill="#2a3647">+${checkedInitials.length - 3}</text>
+        </svg>
+      </div>
+    `;
 }
 
 function openCategories() {
@@ -294,10 +349,7 @@ function openCategories() {
       categories.classList.add('show');
     }, 10);
   } else {
-    categories.classList.remove('show');
-    setTimeout(() => {
-      categories.innerHTML = '';
-    }, 300);
+    hideDropdownCategories();
   }
   dropdownIconCategories.classList.toggle("open");
 }
@@ -328,13 +380,7 @@ function showUserStoryInInput() {
 }
 
 function clearTask() {
-  document.getElementById("task-title").value = '';
-  document.getElementById("task-description").value = '';
-  dateInput.value = '';
-  urgentButton.classList.remove("selected");
-  mediumButton.classList.remove("selected"); 
-  lowButton.classList.remove("selected");
-  createTaskButton.classList.add("d-none");
+  clearAllInputFields();
   changeUrgentBtnToDefault();
   changeMediumBtnToDefault();
   changeLowBtnToDefault();
@@ -343,6 +389,16 @@ function clearTask() {
   clearSubtaskInput()
   setDefaultToCategoryDropdown();
   setDefaultToContactDropdown();
+}
+
+function clearAllInputFields() {
+  document.getElementById("task-title").value = '';
+  document.getElementById("task-description").value = '';
+  dateInput.value = '';
+  urgentButton.classList.remove("selected");
+  mediumButton.classList.remove("selected");
+  lowButton.classList.remove("selected");
+  createTaskButton.disabled = true;
 }
 
 function setDefaultToContactCheckboxes() {
@@ -389,10 +445,8 @@ function enableCreateTaskButton() {
   const dueDate = document.getElementById("task-due-date").value.trim();
 
   if (title !== "" && dueDate !== "" && category !== "Select task category") {
-    createTaskButton.classList.remove("d-none");
     createTaskButton.disabled = false;
   } else {
-    createTaskButton.classList.add("d-none");
     createTaskButton.disabled = true;
-}
+  }
 }
