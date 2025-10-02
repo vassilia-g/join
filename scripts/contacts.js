caca61fc936e63328f2315a86decfa85aea2d0e7
Dustin Condello <dustincondello@googlemail.com>
2025-10-02 18:01:39 +0200

    update more color for avatar

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -111,7 +111,7 @@ function getInitials(name) {
 }
 
 function getRandomColor() {
-    const colors = ["#FF6B6B", "#4ECDC4", "#FFD93D", "#6A5ACD", "#FF8C00", "#20B2AA"];
+    const colors = ["#FF7A00", "#FF5EB3", "#6E52FF", "#9327FF", "#00BEE8", "#1FD7C1", "#FF745E", "#FFA35E", "#FC71FF", "#FFC701", "#0038FF", "#C3FF2B", "#FFE62B", "#FF4646", "#FFBB2B",];
     return colors[Math.floor(Math.random() * colors.length)];
 }
 
