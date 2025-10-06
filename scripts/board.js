8bc1d873240440eff73091a74b365a19ee0b7662
Marco Rößler <118630069+MR-1985@users.noreply.github.com>
2025-10-06 09:48:43 +0200

    added h3 style to global.css; added style for tasks in board.css; added code for tasks IDEA, without java scipt code; added drag n drop function

--- scripts/board.js
+++ scripts/board.js
@@ -41,3 +41,19 @@ function closeOverlay() {
   const overlayBackgroundRef = document.getElementById('overlay-background');
   overlayBackgroundRef.classList.add('d-none');
 }
+
+// Drag and Drop Funktionen
+function allowDrop(event) {
+  event.preventDefault();
+}
+
+function drag(event) {
+  event.dataTransfer.setData("text", event.target.id);
+}
+
+function drop(event) {
+  event.preventDefault();
+  const data = event.dataTransfer.getData("text");
+  const draggedElement = document.getElementById(data);
+  event.target.appendChild(draggedElement);
+}
