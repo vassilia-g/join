d713fd93299c8c4476f0a91eca746198ba005cb7
Vassilia <vassilia@gerodimos.com>
2025-11-02 12:04:22 +0100

    refactorizing the template

--- scripts/templates/template.js
+++ scripts/templates/template.js
@@ -1,7 +1,7 @@
 function showSidebar() {
     return `
     <div class="logo"><a href="./summary.html">
-        <img src="../assets/imgs/join-logo/Logo-white.svg" /></a>
+        <img src="../assets/imgs/join-logo/logo-white.svg" /></a>
     </div>
     <div class="sidebar-nav">
         <a class="nav" href="./summary.html">
@@ -71,7 +71,7 @@ function showHeader(initials = 'GU') {
 function showSidebarBeforeLogin() {
     return `
         <div class="logo">
-            <img src="../assets/imgs/join-logo/Logo-white.svg" />
+            <img src="../assets/imgs/join-logo/logo-white.svg" />
         </div>
         <div class="sidebar-nav">
             <a class="nav" href="../index.html">
